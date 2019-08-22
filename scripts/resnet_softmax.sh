2da6f08b9235eece02e0af35ed209ca6261c3bd4
pengbaoyun <pengbaoyun@sensetime.com>
2019-08-22 23:26:27 +0800

    d

--- scripts/resnet_softmax.sh
+++ scripts/resnet_softmax.sh
@@ -1,9 +1,8 @@
-##
 # $1: partitions;
-# $2: gpus-id;
+# $2: network architecture;
 # $3: depth;
-# $4: -e for evaluate, -r for resume;
-# e.g. ./resnet_softmax.sh Test 1 32 -e
+# $4: dataset, cifar10 or cifar100
+# e.g. ./resnet_softmax.sh Test resnet 32 cifar100
 MV2_USE_CUDA=1 MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=0 \
 srun --mpi=pmi2 -p $1 -n1 --gres=gpu:1 --ntasks-per-node=1 --job-name=spl_distill \
 python main_ce.py \
